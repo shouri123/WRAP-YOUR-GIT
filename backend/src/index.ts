81f3a042b4c8bab686c7a29984543d821ac33e5d
Shouri Chakraborty <107758587+shouri123@users.noreply.github.com>
2025-12-31 18:40:33 +0530

    Final Changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -175,6 +175,11 @@ function getColorForLanguage(language: string): string {
     return colors[language] || "#ccc";
 }
 
-app.listen(5000, () => {
-    console.log("Backend running on http://localhost:5000");
-});
+if (process.env.VERCEL !== '1') {
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+        console.log(`Backend running on http://localhost:${PORT}`);
+    });
+}
+
+export default app;
